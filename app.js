2603ede49bda5ea47fa33862328b94ab8a90f1ed
sandun7787 <sandunwarnasooreya345@gmail.com>
2024-12-14 21:36:53 +0530

    connect to the data based

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log('by')
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+// Connect to MongoDB
+mongoose.connect("mongodb+srv://admin:IWfvVUoXYFfRtEYy@cluster0.mwlxk.mongodb.net/", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {
+    console.log("Connected to MongoDB successfully!");
+
+    // Start the server after successful DB connection
+    app.listen(3000, () => {
+        console.log("Server is running on port 3000");
+    });
+})
+.catch((err) => {
+    console.error("Failed to connect to MongoDB:", err);
+});
+
+// Middleware
+app.use("/", (req, res, next) => {
+    res.send("It is working");
+});
