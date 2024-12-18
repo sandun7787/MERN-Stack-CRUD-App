7a5fa0ddc96ff672fbc479d12aee12759a6500ec
sandun7787 <sandunwarnasooreya345@gmail.com>
2024-12-18 08:48:08 +0530

    update

--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -4,6 +4,7 @@ const userController = require("../Controllers/UserController");
 
 router.get("/", userController.getAllUser); // GET /users
 router.post("/", userController.addUser);  // POST /users
-router.get("/:id",userController.getById)
-router.get("/:id",userController.updateUser)
+router.get("/:id",userController.getById);
+router.put("/:id",userController.updateUser);
+router.put("/:id", userController.deleteUser);
 module.exports = router;
